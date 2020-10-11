6dc8dadd9364a06a16006a3a59c189a66465a9d7
smazzanti <41154179+smazzanti@users.noreply.github.com>
2020-10-11 17:40:41 +0200

    Update main.py

--- confusion_viz/main.py
+++ confusion_viz/main.py
@@ -3,6 +3,8 @@ from sklearn.metrics import precision_recall_curve
 import plotly.graph_objects as go
 from plotly.offline import init_notebook_mode
 
+from utils import find_closest
+
 
 class ConfusionViz:
     
@@ -228,36 +230,3 @@ class ConfusionViz:
         except:
             self._make_figure()
             self.Figure.write_html(filepath)
-            
-    
-def find_closest(look_in, look_for):
-    """
-    Find elements of array 'look_in' that are as close as possible to the elements of array 'look_for'.
-    Both 'look_in' and 'look_for' are assumed to be sorted in descending order.
-    
-    Args:
-        look_in: iterable
-        look_for: iterable
-        
-    Returns:
-        i_found: list containing indexes of elements that are in 'look_in'
-    """
-
-    i_found = []
-    i_in, i_for = 0, 0
-    midpoint_before = float('Inf')
-
-    while i_for < len(look_for):  
-        midpoint_after = sum(look_in[i_in : i_in + 2]) / 2 if i_in < len(look_in) - 1 else float('-Inf')
-        if look_for[i_for] <= midpoint_after:
-            i_in += 1
-            midpoint_before = midpoint_after
-        elif look_for[i_for] > midpoint_before:
-            i_for += 1
-        else:
-            i_found += [i_in]
-            i_for += 1
-            i_in += 1
-            midpoint_before = midpoint_after 
-
-    return i_found
